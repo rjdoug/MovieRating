9e9383813cd7cd1f2f89544b4fc11cbc60f580a3
Riley Douglas <rileydouglas@outlook.com>
2024-03-08 21:09:37 +1300

    nav bar completed (disgusting colors)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,62 @@
+<script lang="ts">
+	export let data;
+
+	console.log(data.path);
+</script>
+
 <nav>
-    <ul>
-        <li>
-            <a href="/new-rating">New Rating</a>
-        </li>
-        <li>
-            <a href="/test">Test</a>
-        </li>
-    </ul>
+	<ul>
+		<li>
+			<a href="/" class={data.path[0] == '' ? 'selected' : ''}>Home</a>
+		</li>
+		<li>
+			<a href="/new-rating" class={data.path[0] == 'new-rating' ? 'selected' : ''}>New Rating</a>
+		</li>
+	</ul>
 </nav>
 
-<slot/>
+<slot />
 
 <style>
+	:global(body, html) {
+		height: 100%;
+		padding: 0;
+		margin: 0;
+		background-color: #f7f7f7; /* Light gray background */
+		font-family: 'Roboto', sans-serif; /* Material UI default font */
+	}
 
-:global(body, html) {
-        height: 100%;
-        padding: 0;
-        margin: 0;
-        background-color: #f0f0f0; /* Light gray background */
-        font-family: 'Roboto', sans-serif; /* Material UI default font */
-    }
+	.selected {
+		background-color: #5cb55c;
+	}
 
-    nav ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        align-items: center;
-        background-color: #1976d2; 
-        height: 3rem;
-    }
+	nav ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		background-color: #0275dd;
 
-    li {
-        height: 100%;
-    }
+		height: 3rem;
+	}
 
-    nav ul li a {
-        text-decoration: none;
-        color: white;
-        display: block;
-        transition: background-color 0.3s ease;
-        height: 100%;
-        padding: 0 1rem;
-    }
+	li {
+		height: 100%;
+	}
 
-    nav ul li a:hover {
-        background-color: rgba(255, 255, 255, 0.1);
-    }
+	nav ul li a {
+		text-decoration: none;
+		color: white;
+		display: flex;
+		align-items: center;
+		transition: background-color 0.3s ease;
+		height: 100%;
+		padding: 0 1rem;
+	}
 
-
-
-
+	nav ul li:hover,
+	.selected:hover {
+		background-color: #9400e7;
+	}
 </style>
-
---
+++ src/routes/+layout.ts
@@ -0,0 +1,21 @@
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async ({ url }) => {
+	try {
+		// get array of paths
+		console.log(url.pathname);
+
+		// split on each path route. Remove falsey ie ''
+		let pathArr = url.pathname.split('/').filter(Boolean);
+
+		if (!pathArr.length) pathArr = [''];
+
+		return {
+			path: pathArr
+		};
+	} catch (error: any) {
+		// Handle the error here, you can log it or re-throw it if necessary
+		console.error('Error occurred:', error.message);
+		throw error; // Rethrow the error to propagate it up the call stack if needed
+	}
+};
