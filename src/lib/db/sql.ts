955ad842c9d2837a7c8c6b44b47c3d8b751dbf2a
Riley Douglas <rileydouglas@outlook.com>
2024-05-15 15:56:24 +1200

    security commit

--- src/lib/db/sql.ts
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const mysqlconn = await mysql.createConnection({
-	host: '127.0.0.1',
-	user: 'root',
-	password: 'Siennabusk2014',
-	database: 'MovieRating'
-});
