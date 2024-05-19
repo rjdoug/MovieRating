8753f292370cd724a92aa08309d379080fe429ca
Riley Douglas <rileydouglas@outlook.com>
2024-05-20 10:26:00 +1200

    basic layout for individual rating detail page

--- src/routes/ratings/[movieID]/+page.svelte
+++ src/routes/ratings/[movieID]/+page.svelte
@@ -23,17 +23,26 @@
 					{value.label}
 				</category-label>
 				<category-value>
-					{value.value}
+					{value.value}/5
 				</category-value>
 			</category-detail>
 		{/each}
+		<total-rating>
+			<total-label>
+				{data.rating.totalRating.label}
+			</total-label>
+			<total-value>
+				{data.rating.totalRating.value}/5
+			</total-value>
+		</total-rating>
 	</rating-details>
 </main>
 
 <style>
 	main {
 		display: flex;
-		width: 50%;
+		width: 40%;
+		margin-top: 2rem;
 	}
 	movie-poster {
 		display: flex;
@@ -47,5 +56,31 @@
 		display: flex;
 		flex-direction: column;
 		flex: 3;
+		gap: 0.7rem;
+	}
+
+	category-detail {
+		display: flex;
+		margin: 0 2rem;
+		font-size: 1rem;
+	}
+
+	category-label {
+		flex: 1;
+	}
+
+	total-rating {
+		display: flex;
+		flex-direction: column;
+		margin: auto;
+		min-width: 2rem;
+		width: 10rem;
+		height: 5rem;
+		gap: 0.5rem;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.5rem;
+		border: 1px solid black;
+		border-radius: 5px;
 	}
 </style>
